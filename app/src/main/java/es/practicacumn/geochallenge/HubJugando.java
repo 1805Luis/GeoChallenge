2b0d40ad6978636c16c52150484f2f15ef8c1010
Luis <l.zambrana@alumnos.upm.es>
2023-05-10 12:59:44 +0200

    Creacion de todas las actividades de nuestra app

--- app/src/main/java/es/practicacumn/geochallenge/HubJugando.java
+++ app/src/main/java/es/practicacumn/geochallenge/HubJugando.java
@@ -1,54 +1,110 @@
 package es.practicacumn.geochallenge;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import es.practicacumn.geochallenge.Fragmentos.Frag_Brujula;
+import es.practicacumn.geochallenge.Model.UsuarioGymkhana.Gymkhana.Prueba;
 import es.practicacumn.geochallenge.Service.GymkhanaService;
 
 public class HubJugando extends AppCompatActivity implements View.OnClickListener {
-    private Button Vital,Mapa,Brujula,LeerQR;
-    private TextView InformacionActual,id,orden;
+    private Button Auxilios,Mapa, Supervivencia,LeerQR;
+    private TextView InformacionActual,Latitud,Longitud;
+    private String idGymkhana,informacion,Lat,Lon;
+    private int ordenPrueba;
+    private List<Prueba> listPruebas;
+    private FirebaseDatabase GymkhanasRef;
     private int request_code =100;
+    private FloatingActionButton pista;
+    private RelativeLayout Coor;
+    private FrameLayout brujula;
+    private boolean ayuda;
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hub_jugando);
         verificarPermisos();
-        Brujula=findViewById(R.id.brujula);
-        Brujula.setOnClickListener(this);
-        Vital=findViewById(R.id.vital);
-        Vital.setOnClickListener(this);
+        listPruebas= new ArrayList<>();
+        Coor=findViewById(R.id.RCoordenadas);
+        Latitud=findViewById(R.id.Latitud);
+        Longitud=findViewById(R.id.Longitud);
+        brujula=findViewById(R.id.containerBrujula);
+        Supervivencia =findViewById(R.id.Supervivencia);
+        Supervivencia.setOnClickListener(this);
+        Auxilios =findViewById(R.id.vital);
+        Auxilios.setOnClickListener(this);
         Mapa=findViewById(R.id.mapa);
         Mapa.setOnClickListener(this);
         LeerQR=findViewById(R.id.leerQR);
         LeerQR.setOnClickListener(this);
-        orden=findViewById(R.id.ordenh);
-        id=findViewById(R.id.idh);
         InformacionActual=findViewById(R.id.pista);
+        pista=findViewById(R.id.obtenerPista);
+        pista.setOnClickListener(this);
         detenerServicio();
+        lanzarBrujula();
+        ayuda=false;
+        diseño();
+
+    }
+
+    private void diseño() {
+        if(!ayuda){
+            Coor.setVisibility(View.INVISIBLE);
+            RelativeLayout.LayoutParams layoutParams=(RelativeLayout.LayoutParams) brujula.getLayoutParams();
+            layoutParams.addRule(RelativeLayout.BELOW,InformacionActual.getId());
+            brujula.setLayoutParams(layoutParams);
+        }else{
+            RelativeLayout.LayoutParams layoutParams=(RelativeLayout.LayoutParams) brujula.getLayoutParams();
+            layoutParams.addRule(RelativeLayout.BELOW,Coor.getId());
+            Coor.setVisibility(View.VISIBLE);
+            brujula.setLayoutParams(layoutParams);
+        }
+    }
+
+    private void lanzarBrujula() {
+        Frag_Brujula frag_brujula=new Frag_Brujula();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.containerBrujula, frag_brujula);
+        fragmentTransaction.commit();
     }
 
     @RequiresApi(api = Build.VERSION_CODES.M)
@@ -99,8 +155,9 @@ public class HubJugando extends AppCompatActivity implements View.OnClickListene
     @Override
     public void onClick(View view) {
         switch (view.getId()){
-            case R.id.brujula:
-                Intent intent=new Intent(this, Brujula.class);
+            case R.id.Supervivencia:
+                Intent intent=new Intent(getApplicationContext(),ConsejosS.class);
+                intent.putExtra("Voy",1);
                 startActivity(intent);
                 break;
             case R.id.mapa:
@@ -108,19 +165,40 @@ public class HubJugando extends AppCompatActivity implements View.OnClickListene
                 startActivity(intent1);
                 break;
             case R.id.vital:
-                Elige();
+                Intent intent2=new Intent(getApplicationContext(),ConsejosP.class);
+                intent2.putExtra("Voy",1);
+                startActivity(intent2);
                 break;
             case R.id.leerQR:
                 LeerQR();
+                break;
+            case R.id.obtenerPista:
+                Ayuda();
                 break;
                 
         }
     }
 
+    private void Ayuda() {
+        if(listPruebas.size()>0) {
+            ayuda = true;
+            double latitud = listPruebas.get(ordenPrueba - 1).getLatitud();
+            double longitud = listPruebas.get(ordenPrueba - 1).getLongitud();
+            DecimalFormat decimalFormat = new DecimalFormat("#.0000");
+            Lat = decimalFormat.format(latitud);
+            Lon = decimalFormat.format(longitud);
+            Latitud.setText(Lat);
+            Longitud.setText(Lon);
+            diseño();
+        }else{
+            Toast.makeText(this, "No se ha leido ningún codigo QR", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
     private void LeerQR() {
         IntentIntegrator integrator=new IntentIntegrator(HubJugando.this);
         integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE);
-        integrator.setPrompt("Lector");
         integrator.setCameraId(0);
         integrator.setBeepEnabled(true);
         integrator.setBarcodeImageEnabled(true);
@@ -140,47 +218,53 @@ public class HubJugando extends AppCompatActivity implements View.OnClickListene
             Matcher matcher = pattern.matcher(datos);
         
             if(matcher.matches()){
-                String ordenPrueba = datos.split("Orden de la prueba: ")[1].split(" ")[0];
+                ayuda=false;
+                diseño();
+                //Extraer el orden de la prueba
+                String orden = datos.split("Orden de la prueba: ")[1].split(" ")[0];
+                ordenPrueba= Integer.parseInt(orden);
 
                 // Extraer el ID de la Gymkhana
-                String idGymkhana = datos.split("Id_Gymkhana: ")[1].split(" ")[0];
+                idGymkhana = datos.split("Id_Gymkhana: ")[1].split(" ")[0];
+                leerPruebas();
 
                 // Extraer la información
-                String informacion = datos.split("Informacion: ")[1];
+                informacion = datos.split("Informacion: ")[1];
 
-                orden.setText(ordenPrueba);
-                id.setText(idGymkhana);
                 InformacionActual.setText(informacion);
+
             }else{
                 Toast.makeText(this, "El código QR no tiene la estructura válida", Toast.LENGTH_SHORT).show();
             }
 
-            
         }
 
+    private void leerPruebas() {
+        GymkhanasRef= FirebaseDatabase.getInstance();
+        DatabaseReference ref= GymkhanasRef.getReference("Gymkhana/"+idGymkhana+"/pruebas");
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                listPruebas.clear();
+                if (snapshot.exists()) {
+                        for(DataSnapshot dataSnapshot: snapshot.getChildren()){
+                            Prueba prueba=dataSnapshot.getValue(Prueba.class);
+                            listPruebas.add(prueba);
+                        }
+                    }else{
+                    Toast.makeText(HubJugando.this, "No existen pruebas para esta gymkhana", Toast.LENGTH_SHORT).show();
+                }
+                }
 
-    private void Elige() {
-        AlertDialog.Builder alerta= new AlertDialog.Builder(this);
-        alerta.setTitle("Tipos de consejos");
-        alerta.setMessage("¿Con cual desea interactuar?")
-                .setCancelable(false)
-                .setPositiveButton("Consejos Supervivencia", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        Intent intent=new Intent(getApplicationContext(),ConsejosS.class);
-                        intent.putExtra("Voy",1);
-                        startActivity(intent);
-                        finish();
-                    }
-                })
-                .setNegativeButton("Primeros Auxilios", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        Intent intent=new Intent(getApplicationContext(),ConsejosP.class);
-                        intent.putExtra("Voy",1);
-                        startActivity(intent);
-                    }
-                });
-        alerta.create().show();
+
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(HubJugando.this, "Error en la base de datos", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
     }
 }
--- app/src/main/java/es/practicacumn/geochallenge/Mapa.java
+++ app/src/main/java/es/practicacumn/geochallenge/Mapa.java
@@ -1,20 +1,89 @@
 package es.practicacumn.geochallenge;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.preference.PreferenceManager;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.osmdroid.api.IGeoPoint;
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.CustomZoomButtonsController;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.Marker;
+import org.osmdroid.views.overlay.compass.CompassOverlay;
 
 public class Mapa extends AppCompatActivity {
-
+    private String Nombre, Lugar, Dificultad, ParticipantesMax, FechaInicio, FechaFin, HoraInicio, HoraFin,Descripcion,UserId;
+    private Marker previous,markerinicial; // Variable para guardar el marcador anterior
+    private IMapController mapController;
+    private MapView map = null;
+    private float startX, startY;
+    private EditText LatitudP, LongitudP;
+    private double LatitudUbicacion, LongitudUbicacion, Latitud,Longitud;
+    private Button Busqueda,Siguiente;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mapa);
+        map = findViewById(R.id.MapView);
+        map.setTileSource(TileSourceFactory.MAPNIK);
+        mapController = map.getController();
+        mapController.setZoom(18.0);
+
         PermisoLocalizacion();
+        getLocalizacion();
+
+        Context ctx = this.getApplicationContext();
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
+        previous = null;
+
+
+        map.getZoomController().setVisibility(CustomZoomButtonsController.Visibility.ALWAYS);
+        map.setMultiTouchControls(true);
+
+
+        //Brujula
+        CompassOverlay compassOverlay = new CompassOverlay(this, map);
+        compassOverlay.enableCompass();
+        map.getOverlays().add(compassOverlay);
+
+        map.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        // Guarda la posición inicial del evento
+                        startX = event.getX();
+                        startY = event.getY();
+                        break;
+                    case MotionEvent.ACTION_UP:
+
+
+                        break;
+                }
+
+                return false;
+            }
+        });
     }
     private void PermisoLocalizacion() {
         int permiso= ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION);
@@ -24,7 +93,35 @@ public class Mapa extends AppCompatActivity {
                 ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
             }
         }
-
     }
+
+    private void getLocalizacion() {
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                if(previous!=null){
+                    previous=null;
+                }
+                if(markerinicial!=null){
+                    map.getOverlays().remove(markerinicial);
+                }
+                IGeoPoint point=new GeoPoint(location.getLatitude(),location.getLongitude());
+                Marker marker = new Marker(map);
+                marker.setPosition((GeoPoint) point);
+                Drawable Picono=getResources().getDrawable(R.drawable.ic_seleccionlugar);
+                marker.setIcon(Picono);
+                marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+                map.getOverlays().add(marker);
+                map.getController().setCenter(point);
+                markerinicial = marker;
+
+            }
+        };
+        int permiso=ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION);
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,100,5,locationListener);
+    }
+
+
 
 }
